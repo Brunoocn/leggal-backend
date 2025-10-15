d9abd53fa2b0eebeabef0a6416379fa8a45cdaef
Bruno Caneo <brunocaneo3@gmail.com>
2025-10-15 16:26:12 -0300

    ✨ feat: inicializando o projeto

---
+++ README.md
@@ -0,0 +1,32 @@
+## 🧪 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node](https://nodejs.org/en/)
+- [Nestjs](https://nestjs.com)
+- [TypeORM](https://typeorm.io/)
+- [Vitest](https://vitest.dev/)
+
+## 🚀 Como executar
+
+Clone o projeto e acesse a pasta do mesmo.
+
+```bash
+$ git clone https://github.com/Brunoocn/leggal-case-backend.git
+$ cd leggal-case-backend.git
+```
+
+Para iniciá-lo, siga os passos abaixo:
+
+```bash
+# subir a aplicação com o docker
+$ docker-compose up -d
+```
+
+O server irá subir na porta http://localhost:3005.
+
+Para acessar a documentação da api, basta acessar http://localhost:3000/api/docs.
+
+Vale lembrar que você deve configurar .env como o .env.sample e o .env.prod como o .env.sample.prod
+
+
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors({
+    origin: '*',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Leggal')
+    .setDescription('API para o teste técnico da Leggal')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3005);
+}
+bootstrap();
